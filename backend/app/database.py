cc1ccd123cd58764c88c3a2dac67d5d705086276
operasisistemintegrasi <operasisistemintegrasi@gmail.com>
2026-08-19 21:15:23 +0700

    feat: test pipeline

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,20 +3,29 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Load file .env jika dijalankan secara lokal (di luar Docker)
 load_dotenv()
 
-# Ambil nilai individu dari environment variable
-DB_USER = os.getenv("MYSQL_USER", "taskuser")
-DB_PASSWORD = os.getenv("MYSQL_PASSWORD", "taskpassword")
-DB_HOST = os.getenv("MYSQL_HOST", "localhost")  # 'localhost' di lokal, 'db-service' di Docker
-DB_PORT = os.getenv("MYSQL_PORT", "3306")
-DB_NAME = os.getenv("MYSQL_DATABASE", "taskflow_db")
+# Cek apakah aplikasi sedang berjalan dalam mode Unit Testing
+TESTING = os.getenv("TESTING", "False").lower() in ("true", "1", "t")
 
-# Susun DATABASE_URL secara dinamis
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if TESTING:
+    # Gunakan SQLite in-memory untuk Pytest
+    DATABASE_URL = "sqlite:///:memory:"
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    # Gunakan MySQL untuk Normal App Runtime
+    DB_USER = os.getenv("MYSQL_USER", "taskuser")
+    DB_PASSWORD = os.getenv("MYSQL_PASSWORD", "taskpassword")
+    DB_HOST = os.getenv("MYSQL_HOST", "localhost")
+    DB_PORT = os.getenv("MYSQL_PORT", "3306")
+    DB_NAME = os.getenv("MYSQL_DATABASE", "taskflow_db")
 
-engine = create_engine(DATABASE_URL)
+    DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
